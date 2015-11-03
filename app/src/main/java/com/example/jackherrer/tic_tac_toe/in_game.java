c30806d24eb5305b9d3de5446b1647088f4f607f
Jack-Herrer <michielvanderlist@yahoo.co.uk>
2015-11-03 13:34:08 +0100

    standard android project files

---
+++ app/src/main/java/com/example/jackherrer/tic_tac_toe/in_game.java
@@ -0,0 +1,13 @@
+package com.example.jackherrer.tic_tac_toe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class in_game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_game);
+    }
+}
